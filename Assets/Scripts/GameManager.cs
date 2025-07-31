0c319534397464a64711865c51d20ba5eee33185
NguyenThanhHung25092004 <dinhmenhvai2004@gmail.com>
2025-07-31 10:21:51 +0700

    31/07/2025 (10:21)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,8 @@ public class GameManager : MonoBehaviour
     private void Awake()
     {
         Application.targetFrameRate = 60;
+        PlayerPrefs.SetInt("AdQuesScore", 0);
+        PlayerPrefs.SetInt("AdQuesLevelScore", 0);
         DontDestroyOnLoad(gameObject);
     }
 }
